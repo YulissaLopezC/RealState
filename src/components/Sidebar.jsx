63eeb5f3bef797ef6786bd9ec238866afb4968e2
Yulissa Lopez <lyulissa5@gmail.com>
2021-10-11 20:44:16 -0500

    creacion de hook useActiveRoute para ruta activa

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
 import '../styles/styles.css'
-import { Link } from 'react-router-dom'
+import { Link, } from 'react-router-dom'
+import useActiveRoute from 'hooks/useActiveRoute'
+
+
 
 const Sidebar = () => {
     return (
         <div className="flex flex-col w-72 items-center pt-6">
-            <div className="flex title_style mb-10"><i className="fas fa-home title_style"></i>ouseHold</div>
+            <Link to="/admin">
+                <div className="flex title_style mb-10"><i className="fas fa-home title_style"></i>ouseHold</div>
+            </Link>
             <nav className="w-full h-1/4 bg-white mt-3">
                 <ul className="flex flex-col items-center w-full h-full">
-
-                    <Link to="/admin">
-                    <li className="items_menu">Home</li>
-                    </Link>
-                    <Link to="/admin/clientes">
-                        <li className="items_menu">Customers</li>
-                    </Link>
-                    <Link to="/admin/casas">
-                        <li className="items_menu">Houses</li>
-                    </Link>
+                    <Ruta ruta="/admin/perfil" nombre="perfil"/>
+                    <Ruta ruta="/admin/clientes" nombre="Customer"/>
+                    <Ruta ruta="/admin/casas" nombre="Houses"/>
                 </ul>
             </nav>
             
         </div>
+    )
+}
+
+const Ruta = ({ruta, nombre}) =>{
+
+    const isActive = useActiveRoute(ruta);
+
+    return(
+    <Link to={ruta}>
+        <li className={`items_menu bg-${isActive ? "indigo-400" : "transparent"}`}>{nombre}</li>
+    </Link>
     )
 }
 
---
+++ src/pages/admin/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div>
+            Pefil
+        </div>
+    )
+}
+
+export default Profile
